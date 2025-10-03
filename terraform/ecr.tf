248c97533b452db189fe5255c2593e928a682f4d
Ziad <zizoo156@gmail.com>
2025-10-03 19:27:47 +0000

    Updating codes......

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "app_repo" {
-  name = "my-node-app-repo"
+  name                 = "my-node-app-repo"
   image_tag_mutability = "IMMUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
